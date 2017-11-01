34cdb519e3774957ccfbb83eb97cd6e671f1dd52
jack <srujanjack@gmail.com>
2017-10-31 22:59:36 -0500

    fixes

--- src/status/lib/saveStatus.js
+++ src/status/lib/saveStatus.js
@@ -3,12 +3,14 @@ var mongoDbConnection = require('myna-server').mongoDb;
 var ValidationError = require('myna-server').ValidationError;
 var updateStatusLocation = require('myna-global').updateStatusLocation;
 var getStatusWithReplies = require('myna-global').getStatusWithReplies;
+var saveImagesOnS3 = require('myna-global').saveImagesOnS3;
+
 var _ = require('lodash');
 var ObjectID = require('mongodb').ObjectID;
+var Promise = require('bluebird');
 
 /* {
  "status": "hello world4",
- "media":[],
  "userId":"12345",
  "userName": "Jack",
  "location":[-77.18621789486043,
@@ -18,45 +20,47 @@ var ObjectID = require('mongodb').ObjectID;
  "statusGroupId" : null | "statusId"
  }*/
 app.post('/myna/status/saveStatus', function (req, res) {
-    var status = req.body;
+    var status = JSON.parse(req.body.data);
     status.timeStamp = Math.floor(Date.now());
     status.condition = 1;
 
-    if (req.body.type == 'text') {
+    if (status.type == 'text') {
         status.replyCount = 0;
     }
+    status.emotions = {
+        "like": [],
+        "dislike": []
+    };
 
     updateStatusLocation(status)
-        .then(function (status1) {
-            mongoDbConnection(function (databaseConnection) {
-                databaseConnection.collection('status', function (error, collection) {
-                    status1.emotions = {
-                        "like": [],
-                        "dislike": []
-                    };
-
-                    if (req.body.type == 'commentText') {
-                        collection.update({_id: ObjectID(req.body.statusGroupId)}, {$inc: {replyCount: 1}}, function (err, records) {
-                            collection.insert(status1, function (err, records) {
-                                getStatusWithReplies(req.body.statusGroupId, req.body.userId)
-                                    .then(function (statusres) {
-                                        res.status(200).send(statusres);
-                                    }, function (statusError) {
-                                        throw new ValidationError(JSON.stringify(statusError), 500);
-                                    });
-                            })
+    .then((result) => saveImagesOnS3(result, req.files))
+    .then((result) => {
+        mongoDbConnection(function (databaseConnection) {
+            databaseConnection.collection('status', function (error, collection) {
+                if (result.type == 'commentText') {
+                    collection.update({_id: ObjectID(result.statusGroupId)}, {$inc: {replyCount: 1}}, function (err, records) {
+                        collection.insert(result, function (err, records) {
+                            getStatusWithReplies(result.statusGroupId, result.userId)
+                                .then(function (statusres) {
+                                    res.status(200).send(statusres);
+                                }, function (statusError) {
+                                    throw new ValidationError(JSON.stringify(statusError), 500);
+                                });
                         })
-                    }
-                    else {
-                        collection.insert(status1, function (err, records) {
-                            if (err)
-                                throw new ValidationError(JSON.stringify(err), 500);
-                            res.status(200).send(records);
-                        })
-                    }
-                })
-            });
-        }, function (updateError) {
-            throw new ValidationError('Error in updating status location', 500)
+                    })
+                }
+                else {
+                    collection.insert(result, function (err, records) {
+                        if (err)
+                            throw new ValidationError(JSON.stringify(err), 500);
+                        res.status(200).send(records);
+                    })
+                }
+            })
         });
+    })
+    .catch(function(err) {
+        //Write error logs
+        res.status(505).send(err);
+    });
 });
