6522caef59c30956b0d0562ba7cdb780fdbc9052
jack <srujanjack@gmail.com>
2017-11-28 18:24:30 -0600

    fixes

--- src/status/lib/getStatus.js
+++ src/status/lib/getStatus.js
@@ -6,6 +6,6 @@ app.get('/myna/status/getStatus', function (req, res) {
 
     getStatusWithReplies(req.query.statusId, req.query.userId)
         .then(statusres => res.status(200).send(statusres))
-        .catch(statusError => res.status(500).send(statusError.stack));
+        .catch(statusError => res.status(500).send(JSON.stringify(statusError)));
 
 });
